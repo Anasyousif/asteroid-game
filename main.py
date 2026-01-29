f2781a1ea677747e51200499a913f2423a89b940
Anas <anasyousif182@gmail.com>
2026-01-29 07:00:55 -0500

    finished rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,13 +10,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
-        log_state()
-        screen.fill("black")
-        pygame.display.flip()
+        # 1. Calculate time first
+        dt = clock.tick(60) / 1000
+
+        # 2. Check for the Quit event
         for event in pygame.event.get():
-            pass  
-        dt = clock.tick(60)/1000             
+            if event.type == pygame.QUIT:
+                return
+
+        # 3. Update the player state
+        player.update(dt)
+        log_state()
+        # 4. Draw to the screen
+        screen.fill("black")
+        player.draw(screen) 
+        pygame.display.flip()           
 if __name__ == "__main__":
     main()
